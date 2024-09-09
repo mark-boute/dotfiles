0554d0b824b0429517afa15d45c6234405f3c380
Mark Boute <bmark0702@gmail.com>
2024-09-09 17:06:34 +0200

    dconf settings

--- home-manager/desktop/gnome-settings/default.nix
+++ home-manager/desktop/gnome-settings/default.nix
@@ -18,70 +18,72 @@ in {
   dconf.enable = true;
     dconf.settings = {
 
-      "/org/gnome/desktop" = {
-        # dark theme
-        interface = {
-          color-scheme = "prefer-dark";
-          text-scaling-factor = 1.2;
-        };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        text-scaling-factor = 1.20;
+      };
         
-        # set background
-        background = {
-          color-shading-type = "solid";
-          picture-options = "zoom";
-          picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
-          picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-d.svg";
-          primary-color = "#241f31";
-          secondary-color = "#000000";
-        };
-
-        # set screensaver
-        screensaver = {
-          color-shading-type = "solid";
-          picture-options = "zoom";
-          picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
-          primary-color = "#241f31";
-          secondary-color = "#000000";
-        };
-
-        "wm/keybindings/toggle-fullscreen" = ["<Super>f"];
+      # set background
+      "org/gnome/desktop/background" = {
+        color-shading-type = "solid";
+        picture-options = "zoom";
+        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
+        picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-d.svg";
+        primary-color = "#241f31";
+        secondary-color = "#000000";
       };
 
-      "/org/gnome/settings-daemon/plugins/media-keys" = {
-        www = ["<Super>w"];
+      # set screensaver
+      "org/gnome/desktop/screensaver" = {
+        color-shading-type = "solid";
+        picture-options = "zoom";
+        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
+        primary-color = "#241f31";
+        secondary-color = "#000000";
+      };
 
-        terminal = [ "<Super>t" ];
+      "org/gnome/mutter" = {
+        dynamic-workspaces = true;
+        edge-tiling = true;
+      };
 
+      "org/gnome/desktop/wm/keybindings" = {
+        move-to-workspace-1 = [ "<Shift><Super>1" ];
+        move-to-workspace-2 = [ "<Shift><Super>2" ];
+        move-to-workspace-3 = [ "<Shift><Super>3" ];
+        move-to-workspace-4 = [ "<Shift><Super>4" ];
+        move-to-workspace-5 = [ "<Shift><Super>5" ];
+        
+        switch-to-workspace-1 = [ "<Super>1" ];
+        switch-to-workspace-2 = [ "<Super>2" ];
+        switch-to-workspace-3 = [ "<Super>3" ];
+        switch-to-workspace-4 = [ "<Super>4" ];
+        switch-to-workspace-5 = [ "<Super>5" ];
+
+        toggle-fullscreen = [ "<Super>f" ];
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys" = {
         decrease-text-size = [ "<Control><Alt>minus" ];
         increase-text-size = [ "<Control><Alt>equal" ];
+        www = [ "<Super>w" ];
+      };
 
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        binding = "<Super>t";
+        command = "kgx";
+        name = "Terminal";
+      };
 
-        # workspace navigation
-        switch-to-workspace-1 = ["<Super>1"];
-        switch-to-workspace-2 = ["<Super>2"];
-        switch-to-workspace-3 = ["<Super>3"];
-        switch-to-workspace-4 = ["<Super>4"];
-        switch-to-workspace-5 = ["<Super>5"];
-
-        move-to-workspace-1 = ["<Super><Shift>1"];
-        move-to-workspace-2 = ["<Super><Shift>2"];
-        move-to-workspace-3 = ["<Super><Shift>3"];
-        move-to-workspace-4 = ["<Super><Shift>4"];
-        move-to-workspace-5 = ["<Super><Shift>5"];
-
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+        binding = "<Super>e";
+        command = "nautilus";
+        name = "Files";
       };
 
       "org/gnome/desktop/peripherals/keyboard" = {
         numlock-state = true;
       };
-
-      "org/gnome/desktop/interface" = {
-        clock-show-date = "true";
-        clock-show-seconds = "true";
-      };
-
-
     };
-
   };
 }
