cc7ea055549721c39918ac61144592ac3afebb9c
Mark Boute <bmark0702@gmail.com>
2024-09-11 11:11:48 +0200

    try to fix shortcuts issue

--- home-manager/desktop/gnome-settings/default.nix
+++ home-manager/desktop/gnome-settings/default.nix
@@ -52,13 +52,11 @@ in {
         move-to-workspace-2 = [ "<Shift><Super>2" ];
         move-to-workspace-3 = [ "<Shift><Super>3" ];
         move-to-workspace-4 = [ "<Shift><Super>4" ];
-        move-to-workspace-5 = [ "<Shift><Super>5" ];
         
         switch-to-workspace-1 = [ "<Super>1" ];
         switch-to-workspace-2 = [ "<Super>2" ];
         switch-to-workspace-3 = [ "<Super>3" ];
         switch-to-workspace-4 = [ "<Super>4" ];
-        switch-to-workspace-5 = [ "<Super>5" ];
 
         toggle-fullscreen = [ "<Super>f" ];
       };
